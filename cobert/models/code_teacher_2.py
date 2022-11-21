9e891a2acdadb7958741fb48c9f0dc0d927d73d9
mct10 <cmeng9@jhu.edu>
2022-11-21 00:32:10 -0500

    revise to code teacher 2

--- cobert/models/code_teacher_2.py
+++ cobert/models/code_teacher_2.py
@@ -9,23 +9,20 @@ import torch.nn.functional as F
 import torch.distributed as dist
 
 from fairseq.data.data_utils import compute_mask_indices
-from fairseq.distributed import fsdp_wrap
 from fairseq.models import register_model, BaseFairseqModel
-from fairseq.models.data2vec import Data2VecAudioConfig, get_annealed_rate
+from fairseq.models.data2vec import Data2VecAudioConfig
 from fairseq.models.transformer import Embedding
-from fairseq.models.wav2vec import pad_to_multiple, TransformerSentenceEncoderLayer, make_conv_pos
 from fairseq.modules import LayerNorm, PositionalEmbedding, SamePad, TransposeLast, EMAModuleConfig, EMAModule
-from fairseq.modules.checkpoint_activations import checkpoint_wrapper
-from fairseq.modules.conformer_layer import ConformerWav2Vec2EncoderLayer
-from fairseq.modules.transformer_sentence_encoder import init_bert_params
-from fairseq.tasks.speech_code_pretraining import SpeechCodePretrainingTask, SpeechCodePretrainingConfig
 from fairseq.utils import index_put
+
+from .modules.code_encoder import TransformerEncoder
+from ..tasks.code_teacher_2_pretraining import CodeTeacher2PretrainingConfig
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
-class Data2VecCodeConfig(Data2VecAudioConfig):
+class CodeTeacher2Config(Data2VecAudioConfig):
     no_scale_embedding: bool = field(
         default=False,
         metadata={"help": "not scale embedding"},
@@ -49,9 +46,15 @@ class Data2VecCodeConfig(Data2VecAudioConfig):
     )
 
 
-@register_model("data2vec_code", dataclass=Data2VecCodeConfig)
-class Data2VecCodeModel(BaseFairseqModel):
-    def __init__(self, cfg: Data2VecCodeConfig, task_cfg: SpeechCodePretrainingConfig, source_dict):
+def get_annealed_rate(start, end, curr_step, total_steps):
+    r = end - start
+    pct_remaining = 1 - curr_step / total_steps
+    return end - r * pct_remaining
+
+
+@register_model("code_teacher_2", dataclass=CodeTeacher2Config)
+class CodeTeacher2(BaseFairseqModel):
+    def __init__(self, cfg: CodeTeacher2Config, task_cfg: CodeTeacher2PretrainingConfig, source_dict):
         super().__init__()
 
         self.cfg = cfg
@@ -118,7 +121,6 @@ class Data2VecCodeModel(BaseFairseqModel):
         padding_idx = dictionary.pad()
         return Embedding(num_embeddings, embed_dim, padding_idx)
 
-    # copy from data2vec_audio
     def make_ema_teacher(self):
         ema_config = EMAModuleConfig(
             ema_decay=self.cfg.ema_decay,
@@ -137,7 +139,6 @@ class Data2VecCodeModel(BaseFairseqModel):
             skip_keys=skip_keys,
         )
 
-    # copy from data2vec_audio
     def set_num_updates(self, num_updates):
         super().set_num_updates(num_updates)
 
@@ -161,7 +162,6 @@ class Data2VecCodeModel(BaseFairseqModel):
 
         self.num_updates = num_updates
 
-    # copy from data2vec_audio
     def state_dict(self, destination=None, prefix="", keep_vars=False):
         state = super().state_dict(destination, prefix, keep_vars)
 
@@ -170,7 +170,6 @@ class Data2VecCodeModel(BaseFairseqModel):
 
         return state
 
-    # copy from data2vec_audio
     def _load_from_state_dict(self, state_dict, prefix, *args, **kwargs):
         if self.ema is not None:
             k = prefix + "_ema"
@@ -180,7 +179,7 @@ class Data2VecCodeModel(BaseFairseqModel):
         return super()._load_from_state_dict(state_dict, prefix, *args, **kwargs)
 
     @classmethod
-    def build_model(cls, cfg: Data2VecCodeConfig, task: SpeechCodePretrainingTask):
+    def build_model(cls, cfg: CodeTeacher2Config, task: CodeTeacher2PretrainingConfig):
         return cls(cfg, task.cfg, task.source_dictionary)
 
     def apply_mask(
@@ -190,9 +189,6 @@ class Data2VecCodeModel(BaseFairseqModel):
         mask_indices=None,
         mask_channel_indices=None,
     ):
-        """
-        basically copy from data2vec audio. but remove channel masking.
-        """
         B, T, C = x.shape
 
         if self.mask_prob > 0:
@@ -496,178 +492,3 @@ class Data2VecCodeModel(BaseFairseqModel):
             self.encoder.layers = nn.ModuleList(
                 l for i, l in enumerate(self.encoder.layers) if i <= last_layer
             )
-
-
-class TransformerEncoder(nn.Module):
-    def build_encoder_layer(self, args: Data2VecCodeConfig):
-        if args.layer_type == "transformer":
-            layer = TransformerSentenceEncoderLayer(
-                embedding_dim=self.embedding_dim,
-                ffn_embedding_dim=args.encoder_ffn_embed_dim,
-                num_attention_heads=args.encoder_attention_heads,
-                dropout=self.dropout,
-                attention_dropout=args.attention_dropout,
-                activation_dropout=args.activation_dropout,
-                activation_fn=args.activation_fn,
-                layer_norm_first=args.layer_norm_first,
-            )
-        elif args.layer_type == "conformer":
-            layer = ConformerWav2Vec2EncoderLayer(
-                embed_dim=self.embedding_dim,
-                ffn_embed_dim=args.encoder_ffn_embed_dim,
-                attention_heads=args.encoder_attention_heads,
-                dropout=args.dropout,
-                depthwise_conv_kernel_size=args.depthwise_conv_kernel_size,
-                activation_fn="swish",
-                attn_type=args.attn_type,
-                use_fp16=args.fp16,
-                pos_enc_type="abs",
-            )
-        layer = fsdp_wrap(layer)
-        if args.checkpoint_activations:
-            layer = checkpoint_wrapper(layer)
-        return layer
-
-    def __init__(self, args: Data2VecCodeConfig):
-        super().__init__()
-
-        self.dropout = args.dropout
-        self.embedding_dim = args.encoder_embed_dim
-        self.required_seq_len_multiple = args.required_seq_len_multiple
-
-        if args.no_pos_conv:
-            self.pos_conv = None
-            logger.info(f"No pos_conv is used.")
-        else:
-            logger.info(f"pos_conv is used.")
-            pos_conv_depth = getattr(args, "pos_conv_depth", 1)
-            if pos_conv_depth > 1:
-                num_layers = args.pos_conv_depth
-                k = max(3, args.conv_pos // num_layers)
-
-                def make_conv_block(e, k, g, l):
-                    return nn.Sequential(
-                        *[
-                            nn.Sequential(
-                                nn.Conv1d(
-                                    e,
-                                    e,
-                                    kernel_size=k,
-                                    padding=k // 2,
-                                    groups=g,
-                                ),
-                                SamePad(k),
-                                TransposeLast(),
-                                LayerNorm(e, elementwise_affine=False),
-                                TransposeLast(),
-                                nn.GELU(),
-                            )
-                            for _ in range(l)
-                        ]
-                    )
-
-                self.pos_conv = make_conv_block(
-                    self.embedding_dim, k, args.conv_pos_groups, num_layers
-                )
-
-            else:
-                self.pos_conv = make_conv_pos(
-                    self.embedding_dim,
-                    args.conv_pos,
-                    args.conv_pos_groups,
-                )
-
-        self.layers = nn.ModuleList(
-            [self.build_encoder_layer(args) for _ in range(args.encoder_layers)]
-        )
-        self.layer_norm_first = args.layer_norm_first
-        self.layer_norm = LayerNorm(self.embedding_dim)
-        self.layerdrop = args.encoder_layerdrop
-
-        self.apply(init_bert_params)
-
-    def forward(self, x, padding_mask=None, layer=None):
-        x, layer_results = self.extract_features(x, padding_mask, layer)
-
-        if self.layer_norm_first and layer is None:
-            x = self.layer_norm(x)
-
-        return x, layer_results
-
-    def extract_features(
-            self,
-            x,
-            padding_mask=None,
-            tgt_layer=None,
-            min_layer=0,
-    ):
-
-        if padding_mask is not None:
-            x = index_put(x, padding_mask, 0)
-
-        if self.pos_conv is not None:
-            x_conv = self.pos_conv(x.transpose(1, 2))
-            x_conv = x_conv.transpose(1, 2)
-            x = x + x_conv
-
-        if not self.layer_norm_first:
-            x = self.layer_norm(x)
-
-        # pad to the sequence length dimension
-        x, pad_length = pad_to_multiple(
-            x, self.required_seq_len_multiple, dim=-2, value=0
-        )
-        if pad_length > 0 and padding_mask is None:
-            padding_mask = x.new_zeros((x.size(0), x.size(1)), dtype=torch.bool)
-            padding_mask[:, -pad_length:] = True
-        else:
-            padding_mask, _ = pad_to_multiple(
-                padding_mask, self.required_seq_len_multiple, dim=-1, value=True
-            )
-        x = F.dropout(x, p=self.dropout, training=self.training)
-
-        # B x T x C -> T x B x C
-        x = x.transpose(0, 1)
-
-        layer_results = []
-        r = None
-        for i, layer in enumerate(self.layers):
-            dropout_probability = np.random.random() if self.layerdrop > 0 else 1
-            if not self.training or (dropout_probability > self.layerdrop):
-                x, (z, lr) = layer(
-                    x, self_attn_padding_mask=padding_mask, need_weights=False
-                )
-                if i >= min_layer:
-                    layer_results.append((x, z, lr))
-            if i == tgt_layer:
-                r = x
-                break
-
-        if r is not None:
-            x = r
-
-        # T x B x C -> B x T x C
-        x = x.transpose(0, 1)
-
-        # undo paddding
-        if pad_length > 0:
-            x = x[:, :-pad_length]
-
-            def undo_pad(a, b, c):
-                return (
-                    a[:-pad_length],
-                    b[:-pad_length] if b is not None else b,
-                    c[:-pad_length],
-                )
-
-            layer_results = [undo_pad(*u) for u in layer_results]
-
-        return x, layer_results
-
-    def max_positions(self):
-        """Maximum output length supported by the encoder."""
-        return self.args.max_positions
-
-    def upgrade_state_dict_named(self, state_dict, name):
-        """Upgrade a (possibly old) state dict for new versions of fairseq."""
-        return state_dict
